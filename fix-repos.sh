c5eef02b769c952cabea1ed0f01c1d245f78e872
Mike Holzinger <mike.holzinger@gmail.com>
2018-05-16 20:07:51 +0000

    Create fix-repos.sh

---
+++ fix-repos.sh
@@ -0,0 +1,8 @@
+!#/bin/bash
+
+sudo sed -i 's/archive.ubuntu/old-releases.ubuntu/' /etc/apt/sources.list
+sudo apt-get clean
+sudo mv /var/lib/apt/lists /tmp
+sudo mkdir -p /var/lib/apt/lists/partial
+sudo apt-get clean 
+sudo apt-get update
